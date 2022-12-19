c294d3e28c270f67a1650cb965a2122447f7a332
sa8a <jpkrllll@yahoo.co.jp>
2022-12-19 18:27:37 +0900

    add: とりあえずFirestoreのデータを表示

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/constants/colors.dart';
 import 'package:todo_app/model/todo.dart';
@@ -21,111 +22,152 @@ class _HomeState extends State<Home> {
     super.initState();
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Stack(
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 15,
-            ),
-            child: Column(
-              children: [
-                searchBox(),
-                Expanded(
-                  child: ListView(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.only(
-                          top: 50,
-                          bottom: 20,
-                        ),
-                        child: const Text(
-                          'All ToDos',
-                          style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ),
-                      for (ToDo todo in _foundToDo.reversed)
-                        ToDoItem(
-                          todo: todo,
-                          onToDoChanged: _handleToDoChange,
-                          onDeleteItem: _deleteToDoItem,
-                        ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.only(
-                      bottom: 20,
-                      right: 20,
-                      left: 20,
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 20,
-                      vertical: 5,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Colors.grey,
-                          offset: Offset(0.0, 0.0),
-                          blurRadius: 10.0,
-                          spreadRadius: 0.0,
-                        ),
-                      ],
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: TextField(
-                      controller: _todoController,
-                      decoration: const InputDecoration(
-                        hintText: 'Add a new todo item',
-                        border: InputBorder.none,
-                      ),
-                    ),
-                  ),
+      body: StreamBuilder<QuerySnapshot>(
+        // streamの設定部分で、前述したデータの取得処理を記載
+        // Cloud FirestoreからStreamでデータを取得
+        // FirebaseFirestore.instanceでCloud Firestoreのデータベースのインスタンスを取得します。
+        // collection(~)でコレクションを選択
+        // orderBy(~)でフィールドの値にて並べ替え
+        // 最後にsnapshots()でStreamにてデータの取得を行う
+        stream: FirebaseFirestore.instance
+            .collection('todo')
+            .orderBy('createdAt')
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Text('エラーが発生しました');
+          }
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          final list = snapshot.requireData.docs
+              .map<String>((DocumentSnapshot document) {
+            final documentData = document.data()! as Map<String, dynamic>;
+            return documentData['todoText']! as String;
+          }).toList();
+
+          final reverseList = list.reversed.toList();
+
+          return ListView.builder(
+            itemCount: reverseList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Center(
+                child: Text(
+                  reverseList[index],
+                  style: const TextStyle(fontSize: 20),
                 ),
-                Container(
-                  margin: const EdgeInsets.only(
-                    bottom: 20,
-                    right: 20,
-                  ),
-                  child: ElevatedButton(
-                    child: const Text(
-                      '+',
-                      style: TextStyle(
-                        fontSize: 40,
-                      ),
-                    ),
-                    onPressed: () {
-                      _addToDoItem(_todoController.text);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: tdBlue,
-                      minimumSize: const Size(60, 60),
-                      elevation: 10,
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ],
+              );
+            },
+          );
+        },
       ),
+
+      // body: Stack(
+      //   children: [
+      //     Container(
+      //       padding: const EdgeInsets.symmetric(
+      //         horizontal: 20,
+      //         vertical: 15,
+      //       ),
+      //       child: Column(
+      //         children: [
+      //           searchBox(),
+      //           Expanded(
+      //             child: ListView(
+      //               children: [
+      //                 Container(
+      //                   margin: const EdgeInsets.only(
+      //                     top: 50,
+      //                     bottom: 20,
+      //                   ),
+      //                   child: const Text(
+      //                     'All ToDos',
+      //                     style: TextStyle(
+      //                       fontSize: 30,
+      //                       fontWeight: FontWeight.w500,
+      //                     ),
+      //                   ),
+      //                 ),
+      //                 for (ToDo todo in _foundToDo.reversed)
+      //                   ToDoItem(
+      //                     todo: todo,
+      //                     onToDoChanged: _handleToDoChange,
+      //                     onDeleteItem: _deleteToDoItem,
+      //                   ),
+      //               ],
+      //             ),
+      //           )
+      //         ],
+      //       ),
+      //     ),
+      //     Align(
+      //       alignment: Alignment.bottomCenter,
+      //       child: Row(
+      //         children: [
+      //           Expanded(
+      //             child: Container(
+      //               margin: const EdgeInsets.only(
+      //                 bottom: 20,
+      //                 right: 20,
+      //                 left: 20,
+      //               ),
+      //               padding: const EdgeInsets.symmetric(
+      //                 horizontal: 20,
+      //                 vertical: 5,
+      //               ),
+      //               decoration: BoxDecoration(
+      //                 color: Colors.white,
+      //                 boxShadow: const [
+      //                   BoxShadow(
+      //                     color: Colors.grey,
+      //                     offset: Offset(0.0, 0.0),
+      //                     blurRadius: 10.0,
+      //                     spreadRadius: 0.0,
+      //                   ),
+      //                 ],
+      //                 borderRadius: BorderRadius.circular(10),
+      //               ),
+      //               child: TextField(
+      //                 controller: _todoController,
+      //                 decoration: const InputDecoration(
+      //                   hintText: 'Add a new todo item',
+      //                   border: InputBorder.none,
+      //                 ),
+      //               ),
+      //             ),
+      //           ),
+      //           Container(
+      //             margin: const EdgeInsets.only(
+      //               bottom: 20,
+      //               right: 20,
+      //             ),
+      //             child: ElevatedButton(
+      //               child: const Text(
+      //                 '+',
+      //                 style: TextStyle(
+      //                   fontSize: 40,
+      //                 ),
+      //               ),
+      //               onPressed: () {
+      //                 _addToDoItem(_todoController.text);
+      //               },
+      //               style: ElevatedButton.styleFrom(
+      //                 backgroundColor: tdBlue,
+      //                 minimumSize: const Size(60, 60),
+      //                 elevation: 10,
+      //               ),
+      //             ),
+      //           )
+      //         ],
+      //       ),
+      //     ),
+      //   ],
+      // ),
     );
   }
 
